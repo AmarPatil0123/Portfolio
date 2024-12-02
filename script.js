53a807b561a79213ed980373d8c14a0d4912b22f
Amar Patil <amarpatil2253@gmail.com>
2024-12-02 15:33:05 +0530

    Make new Design for Project section

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const ham = document.querySelector(".hamburger");
-const navLinks=document.querySelector(".nav-links");
-const navItems=document.querySelectorAll(".nav-links li a")
+const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links li a");
+const checkInfos = document.querySelectorAll(".checkInfo"); // Use querySelectorAll
+const viewInfos = document.querySelectorAll(".view-info"); // Use querySelectorAll
 
 ham.addEventListener("click", () => {
-    ham.classList.toggle("fa-xmark")
+    ham.classList.toggle("fa-xmark");
     navLinks.classList.toggle("move");
 });
 
-navItems.forEach((navItem)=>{
-    navItem.addEventListener("click",()=>{
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
         navLinks.classList.toggle("move");
-        ham.classList.toggle("fa-xmark")
-    })
-})
+        ham.classList.toggle("fa-xmark");
+    });
+});
 
-
+// Add event listener to each project
+checkInfos.forEach((checkInfo, index) => {
+    const viewInfo = viewInfos[index]; // Match the corresponding view-info
+    checkInfo.addEventListener("click", (e) => {
+        e.stopPropagation();
+        viewInfo.classList.toggle("showHide");
+        checkInfo.querySelector(".eye").classList.toggle("fa-xmark");
+    });
+});
